3c94ba960df637e8d708b3d73a53bfb5d8ee0b8c
Conrad <grosserconrad@gmail.com>
2020-03-17 13:45:30 +0100

    Added basic action structure

---
+++ entrypoint.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+# e is for exiting the script automatically if a command fails, u is for exiting if a variable is not set
+# x would be for showing the commands before they are executed
+set -eu
+
+# FUNCTIONS
+# Function for setting up git env in the docker container (copied from https://github.com/stefanzweifel/git-auto-commit-action/blob/master/entrypoint.sh)
+_git_setup ( ) {
+    cat <<- EOF > $HOME/.netrc
+      machine github.com
+      login $GITHUB_ACTOR
+      password $GITHUB_TOKEN
+      machine api.github.com
+      login $GITHUB_ACTOR
+      password $GITHUB_TOKEN
+EOF
+    chmod 600 $HOME/.netrc
+
+    git config --global user.email "actions@github.com"
+    git config --global user.name "GitHub Action"
+}
+
+# Checks if any files are changed
+_git_changed() {
+    [[ -n "$(git status -s)" ]]
+}
+
+# Pushes to the according upstream (origin or input branch)
+_git_push() {
+    if [ -z "$INPUT_BRANCH" ]
+    then
+        git push origin
+    else
+        git push --set-upstream origin "HEAD:$INPUT_BRANCH"
+    fi
+}
+
+# PROGRAM
+# TODO: Add actual commands for compressing
+
+# To keep runtime good, just continue if something was changed
+if _git_changed;
+then
+    # Configure the git environemnt
+    _git_setup
+    echo "Commiting and pushing changes..."
+    # Switch to the actual branch
+    git checkout $INPUT_BRANCH || echo "Problem checking out the specified branch: $INPUT_BRANCH"
+    # Add changes to git
+    git add "${INPUT_FILE_PATTERN}" || echo "Problem adding your files with pattern ${INPUT_FILE_PATTERN}"
+    # Commit and push changes back
+    git commit -m "$INPUT_COMMIT_MESSAGE" --author="$GITHUB_ACTOR <$GITHUB_ACTOR@users.noreply.github.com>" ${INPUT_COMMIT_OPTIONS:+"$INPUT_COMMIT_OPTIONS"}
+    _git_push
+    echo "Changes pushed successfully."
+  fi
+else
+  echo "Nothing to commit. Exiting."
+fi
